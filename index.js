b6976bc9fced6c3f936ef241fd15f783f8b18def
Wilson <wilson.luiz583@gmail.com>
2025-05-11 09:01:23 -0300

    7- commit

--- index.js
+++ index.js
@@ -1,44 +1,61 @@
 let procuraNumero =  0;
-let tentativas = 0;
+let tentativas =0 ;
 
 function gerarNumero(){
-    
 
     procuraNumero = parseInt(Math.random()*100);
 
     console.log(procuraNumero)
 
+    
+
 }
 
 function verificaNumero(event){
-event.preventDefault();
+
+ event.preventDefault();
+
     let palpite = document.getElementById('palpite').value;
 
     if(palpite > 100 || palpite < 1){
 
-        alert('Palpite Inválido');
+        alert('Palpite Inválido o número deve ser menor que 100');
  
         return;
-     
+
     }
 
   if (palpite > procuraNumero){
 
     tentativas++
     document.getElementById('dica').innerText = 'O número é MENOR'
-
+    if(tentativas >= 10){
+        alert(
+            "10 tentativas você perdeu!"
+        ) 
+         
+    } 
 } else if (palpite < procuraNumero){
 
     tentativas++
     document.getElementById('dica').innerText = 'O número é MAIOR'
+    if(tentativas >= 11){
+        alert(
+            "10 tentativas você perdeu!"
+        )
+    }
 
-} else{
+} else {
     document.getElementById('tentativa').innerText = tentativas;
+    document.getElementById('dica').innerText = 'Parabéns! Você acertou!';
+
 }
 
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('formulario').addEventListener('submit', verificaNumero);
+
     gerarNumero();
+
 });
