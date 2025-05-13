85ec0785d35977e70249098f812e6b53f512c13c
Wilson <wilson.luiz583@gmail.com>
2025-05-13 08:52:34 -0300

    9-commit

--- index.js
+++ index.js
@@ -1,61 +1,66 @@
 let procuraNumero =  0;
 let tentativas =0 ;
+let jogoFinalizado = false; //variável para verificar se o jogo foi finalizado.
+
+//Gera um número aleatório
 
 function gerarNumero(){
 
     procuraNumero = parseInt(Math.random()*100);
-
-    console.log(procuraNumero)
-
-    
-
+    console.log(procuraNumero)   
 }
+
+//Verifica se o número é o mesmo sorteado pelo computador
 
 function verificaNumero(event){
 
  event.preventDefault();
 
+ if (jogoFinalizado) {
+    alert('O jogo terminou!Regarregue a página para jogar novamente!')
+    return
+}
+
     let palpite = document.getElementById('palpite').value;
 
+
     if(palpite > 100 || palpite < 1){
+        alert('Palpite Inválido o número deve ser entre 1 a 100');
 
-        alert('Palpite Inválido o número deve ser menor que 100');
- 
-        return;
+        return;}
 
-    }
+    tentativas++ //Incrementa um número
+    document.getElementById('tentativa').innerText = `Tentaiva: ${tentativas}`;
 
-  if (palpite > procuraNumero){
+    if (palpite > procuraNumero){
+    document.getElementById('dica').innerText = 'O número é MENOR';
 
-    tentativas++
-    document.getElementById('dica').innerText = 'O número é MENOR'
     if(tentativas >= 10){
-        alert(
-            "10 tentativas você perdeu!"
-        ) 
-         
+        alert("10 tentativas você perdeu!");
+        jogoFinalizado = true;//Marca o jogo finalizado
+        location.reload();//Regarrega a página     
     } 
+
 } else if (palpite < procuraNumero){
 
-    tentativas++
     document.getElementById('dica').innerText = 'O número é MAIOR'
-    if(tentativas >= 11){
-        alert(
-            "10 tentativas você perdeu!"
-        )
+    if(tentativas >= 10){
+        alert("10 tentativas você perdeu!");
+        jogoFinalizado = true;//Marca o jogo finalizado
+        location.reload();//Recarrega a página
     }
 
 } else {
-    document.getElementById('tentativa').innerText = tentativas;
+    document.getElementById('tentativa').innerText = `Tentaiva: ${tentativas}`;
     document.getElementById('dica').innerText = 'Parabéns! Você acertou!';
+    jogoFinalizado = true;//Marca o jogo finalizado
 
 }
 
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('formulario').addEventListener('submit', verificaNumero);
-
-    gerarNumero();
+     document.addEventListener('DOMContentLoaded', () => {
+     document.getElementById('formulario').addEventListener('submit', verificaNumero);
+     gerarNumero();
 
 });
